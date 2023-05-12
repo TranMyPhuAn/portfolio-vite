6773c76feac747f814c0db8f561619f4a753eb9e
TranMyPhuAn <128568357+TranMyPhuAn@users.noreply.github.com>
2023-05-12 15:27:13 +0700

    tao du an

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+/* eslint-env node */
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'always',
+        semi: true,
+        trailingComma: 'none',
+        tabWidth: 2,
+        endOfLine: 'auto',
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: true
+      }
+    ]
+  }
+};
